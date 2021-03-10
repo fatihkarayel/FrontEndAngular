5e914a577933b50f98924692f9440bae03b12f78
fatih karayel <fkarayel@live.com>
2021-03-10 21:59:13 +0300

    10 Mart 2021

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+
+
+  //köşeli parantez array demek
+  products: Product[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
